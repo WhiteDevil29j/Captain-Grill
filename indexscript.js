b3b0b1ba7ee53dfb0189d16a383ec1bf008cce55
WhiteDevil29 <aryaduhan29@gmail.com>
2021-04-15 17:50:38 +0530

    1st

--- indexscript.js
+++ indexscript.js
@@ -9,6 +9,20 @@ $(document).ready(function () {
         $('#menu-bar').removeClass('fa-times');
         $('.narbar').removeClass('nav-toggle');
 
+
+        $('section').each(function () {
+            let top = $(window).scrollTop();
+            let offset = $(this).offset().top - 400;
+            let height = $(this).height();
+            let id = $(this).attr('id');
+
+            if (top > offset && top < offset + height) {
+                $('.narbar ul li a').removeClass('active');
+                $('.navbar').find('[href = "#${id}"]').addClass('active');
+            }
+
+        });
+
     });
 
     $('.menu .list .btn').click(function () {
